11690b5063a398840dd2005c254525edd69b632c
ZinLinnhtet <mackk5504@gmail.com>
2024-02-07 23:55:08 +0630

    -

--- MiniATM.MVCApp/Program.cs
+++ MiniATM.MVCApp/Program.cs
@@ -1,8 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using MiniATM.MVCApp.EFDbContext;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<AppDbContext>(options =>
+    {
+        string? connectionString = builder.Configuration.GetConnectionString("DbConnection");
+        options.UseSqlServer(connectionString);
+    },
+    ServiceLifetime.Transient,
+    ServiceLifetime.Transient);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
