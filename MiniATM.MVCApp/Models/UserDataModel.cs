0367d073cd3d9702f5b36b5a60ea64c078dcf09a
ZinLinnhtet <mackk5504@gmail.com>
2024-02-07 23:55:04 +0630

    -

---
+++ MiniATM.MVCApp/Models/UserDataModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MiniATM.MVCApp.Models
+{
+    public class UserDataModel
+    {
+        [Key]
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public string NRC { get; set; }
+        public int CardNumber { get; set; }
+        public string Password { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
