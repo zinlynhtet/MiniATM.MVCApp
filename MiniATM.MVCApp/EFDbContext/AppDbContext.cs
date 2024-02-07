efd038e6bc02855870de19d1f6644eec2496629f
ZinLinnhtet <mackk5504@gmail.com>
2024-02-07 23:54:56 +0630

    -

---
+++ MiniATM.MVCApp/EFDbContext/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MiniATM.MVCApp.Models;
+
+namespace MiniATM.MVCApp.EFDbContext
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<UserDataModel> UserData { get; set; }
+    }
+
+}
