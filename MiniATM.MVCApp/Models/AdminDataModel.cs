960ce280364cbeaaebdb1896eeda1b88586e78ea
ZinLinnhtet <mackk5504@gmail.com>
2024-02-08 01:19:54 +0630

    [Complete] Admin Read, Create

---
+++ MiniATM.MVCApp/Models/AdminDataModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MiniATM.MVCApp.Models;
+[Table("AdminTable")]
+public class AdminDataModel
+{
+    [Key]
+    public string AdminID { get; set; } 
+    public string AdminUsername { get; set; }
+    public string AdminPassword { get; set; }
+}
