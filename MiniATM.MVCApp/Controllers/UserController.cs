30fd5d0df73fa7aac8571cd5d5a26ab10fe5959a
ZinLinnhtet <mackk5504@gmail.com>
2024-02-09 03:31:44 +0630

    -

--- MiniATM.MVCApp/Controllers/UserController.cs
+++ MiniATM.MVCApp/Controllers/UserController.cs
@@ -60,8 +60,9 @@ namespace MiniATM.MVCApp.Controllers
         {
             return View("UserWithdrawal");
         }
-        [ActionName("Withdrawal")]
+
         [HttpPost]
+        [ActionName("Withdrawal")]
         public async Task<IActionResult> UserWithdrawal(UserDataModel reqModel)
         {
             var userData = await _context.UserData
@@ -82,6 +83,134 @@ namespace MiniATM.MVCApp.Controllers
             TempData["Message"] = message;
             TempData["IsSuccess"] = result > 0;
             return Redirect("/user");
+        }
+
+        [ActionName("Deposit")]
+        public IActionResult UserDeposit()
+        {
+            return View("UserDeposit");
+        }
+
+        [HttpPost]
+        [ActionName("Deposit")]
+        public async Task<IActionResult> UserDeposit(UserDataModel reqModel)
+        {
+            var userData = await _context.UserData
+                .FirstOrDefaultAsync(b => b.CardNumber == reqModel.CardNumber && b.Password == reqModel.Password);
+
+            if (userData == null)
+            {
+                TempData["Message"] = "Invalid cardNum or pin.";
+                TempData["IsSuccess"] = false;
+                return View("UserDeposit");
+            }
+
+            userData.Balance += reqModel.Balance;
+            _context.UserData.Update(userData);
+            var result = await _context.SaveChangesAsync();
+            var message = result > 0 ? $"Deposit Successful. New Balance: {userData.Balance} $" : "Deposit failed.";
+            TempData["Message"] = message;
+            TempData["IsSuccess"] = result > 0;
+            return Redirect("/user");
+        }
+
+        [HttpPost]
+        [ActionName("Remove")]
+        public async Task<IActionResult> RemoveUser(UserDataModel reqModel)
+        {
+            var userData = await _context.UserData.AsNoTracking().FirstOrDefaultAsync(x => x.UserId == reqModel.UserId);
+            if (userData is null)
+            {
+                TempData["Message"] = "No data found.";
+                TempData["IsSuccess"] = false;
+                goto result;
+            }
+            _context.Remove(userData);
+            int result = _context.SaveChanges();
+            string message = result > 0 ? "Deleting Successful." : "Deleting Failed.";
+            TempData["Message"] = message;
+            TempData["IsSuccess"] = result > 0;
+        result:
+            return Redirect("/user");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ThreeYearsTransitionRecord()
+        {
+            int total = 0;
+            DateTime currentDate = DateTime.Now;
+            DateTime addYear;
+            List<int> years = new List<int>();
+            var userId = HttpContext.Session.GetString("LoginData");
+            var userData = await _context.UserData.FirstOrDefaultAsync(x => x.UserId == userId);
+            for (int i = 0; i < 3; i++)
+            {
+                addYear = currentDate.AddYears(-1 * i);
+                years.Add(addYear.Year);
+            }
+            Random random = new Random();
+            int totalTransitionAmount = 0;
+            bool isSuccess = true;
+            using (var transition = await _context.Database.BeginTransactionAsync())
+            {
+                try
+                {
+                    for (int i = 0; i < 3; i++)
+                    {
+                        for (int j = 0; j < 365; j++)
+                        {
+                            total = i;
+                            decimal Amount = random.Next(100000, 1000000);
+                            decimal income_amount = random.Next(100000, 1000000);
+
+                            DateTime start_date = new DateTime(years[i], 1, 1);
+                            DateTime end_date = new DateTime(years[i], 12, 31);
+                            int range = (end_date - start_date).Days;
+                            int randomDays = random.Next(range);
+                            DateTime randomDate = start_date.AddDays(randomDays);
+                            string transferId = Ulid.NewUlid().ToString();
+                            int transitionAmount = random.Next(1, 101);
+                            totalTransitionAmount += transitionAmount;
+                            var transitionHistory = new TransitionHistoryModel
+                            {
+                                TransferId = transferId,
+                                FromAccount = userData!.CardNumber.ToString()!,
+                                ToAccount = GenerateRandom12DigitNumber().ToString(),
+                                TransferDate = randomDate,
+                                UserId = userId!,
+                                TransitionAmount = transitionAmount,
+                            };
+                            await _context.TransitionHistory.AddAsync(transitionHistory);
+                            await _context.SaveChangesAsync();
+                        }
+                    }
+
+                    if (userData!.Balance < totalTransitionAmount)
+                    {
+                        await transition.RollbackAsync();
+                        goto result;
+                    }
+                    userData.Balance -= totalTransitionAmount;
+                    _context.UserData.Update(userData);
+                    await _context.SaveChangesAsync();
+                    await transition.CommitAsync();
+                }
+                catch (Exception ex)
+                {
+                    isSuccess = false;
+                    await transition.RollbackAsync();
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        result:
+            return Json(new
+            {
+                isSuccess = isSuccess,
+                CurrentBalance = userData!.Balance,
+                TransitionAmount = totalTransitionAmount,
+                RemainingBalance = userData.Balance - totalTransitionAmount,
+                TransitionStatus = isSuccess ? "Transition Successful." : "Transition Failed.",
+            });
         }
     }
 }
