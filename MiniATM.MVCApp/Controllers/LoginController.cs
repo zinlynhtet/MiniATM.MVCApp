6cae797172e17a4eb31d73c9e40e5bd9cf19c40b
ZinLinnhtet <mackk5504@gmail.com>
2024-02-08 22:59:15 +0630

    -

---
+++ MiniATM.MVCApp/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MiniATM.MVCApp.EFDbContext;
+using MiniATM.MVCApp.Models;
+
+namespace MiniATM.MVCApp.Controllers
+{
+    public class LoginController(AppDbContext  context) : Controller
+    {
+        private readonly AppDbContext _context = context;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
