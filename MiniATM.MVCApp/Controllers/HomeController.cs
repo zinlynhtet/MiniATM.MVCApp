def0f74a61361d52fb8ff966083986f483d9344f
ZinLinnhtet <mackk5504@gmail.com>
2024-02-09 03:31:38 +0630

    -

--- MiniATM.MVCApp/Controllers/HomeController.cs
+++ MiniATM.MVCApp/Controllers/HomeController.cs
@@ -1,21 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using MiniATM.MVCApp.Models;
 using System.Diagnostics;
+using MiniATM.MVCApp.EFDbContext;
 
 namespace MiniATM.MVCApp.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly AppDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, AppDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var userId = HttpContext.Session.GetString("LoginData");
+            if (userId == null) return Redirect("/login");
+            var userData = await _context.UserData
+                .FirstOrDefaultAsync(b => b.UserId == userId);
+
+            return View(userData);
         }
 
         public IActionResult Privacy()
