ce67ea03ad2d66ea7618a0664c0d6a7368c7d28e
ZinLinnhtet <mackk5504@gmail.com>
2024-02-08 22:59:29 +0630

    -

---
+++ MiniATM.MVCApp/Models/ResponseMessageModel.cs
@@ -0,0 +1,13 @@
+﻿namespace MiniATM.MVCApp.Models;
+
+public class ResponseMessageModel
+{
+    public ResponseMessageModel() { }
+    public ResponseMessageModel(bool isSuccess, string message)
+    {
+        IsSuccess = isSuccess;
+        Message = message;
+    }
+    public bool IsSuccess { get; set; }
+    public string Message { get; set; }
+}
