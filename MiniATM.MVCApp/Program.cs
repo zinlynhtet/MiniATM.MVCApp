b18c0733296445b53a78fe631e9c034ea7e4fbc7
ZinLinnhtet <mackk5504@gmail.com>
2024-02-08 22:59:35 +0630

    -

--- MiniATM.MVCApp/Program.cs
+++ MiniATM.MVCApp/Program.cs
@@ -5,6 +5,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromSeconds(10);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
 builder.Services.AddDbContext<AppDbContext>(options =>
     {
         string? connectionString = builder.Configuration.GetConnectionString("DbConnection");
@@ -28,9 +35,9 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
-
+app.UseSession();
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=User}/{action=Index}/{id?}");
 
 app.Run();
