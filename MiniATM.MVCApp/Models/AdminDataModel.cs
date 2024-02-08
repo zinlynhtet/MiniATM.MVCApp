b2bef8b7f7751a280f54f66431299e23342900ee
ZinLinnhtet <mackk5504@gmail.com>
2024-02-08 22:59:26 +0630

    -

--- MiniATM.MVCApp/Models/AdminDataModel.cs
+++ MiniATM.MVCApp/Models/AdminDataModel.cs
@@ -1,12 +1,14 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace MiniATM.MVCApp.Models;
-[Table("AdminTable")]
-public class AdminDataModel
+namespace MiniATM.MVCApp.Models
 {
-    [Key]
-    public string AdminID { get; set; } 
-    public string AdminUsername { get; set; }
-    public string AdminPassword { get; set; }
+    [Table("AdminTable")]
+    public class AdminDataModel
+    {
+        [Key]
+        public string AdminID { get; set; }
+        public string AdminUsername { get; set; }
+        public string AdminPassword { get; set; }
+    }
 }
