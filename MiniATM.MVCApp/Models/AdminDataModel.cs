1753f945d10ec9c4e3a06f2f56d9826f63b307f0
ZinLinnhtet <mackk5504@gmail.com>
2024-02-11 23:53:17 +0630

    -

--- MiniATM.MVCApp/Models/AdminDataModel.cs
+++ MiniATM.MVCApp/Models/AdminDataModel.cs
@@ -11,4 +11,15 @@ namespace MiniATM.MVCApp.Models
         public string AdminUsername { get; set; }
         public string AdminPassword { get; set; }
     }
+    public class MessageModel
+    {
+        public MessageModel() { }
+        public MessageModel(bool isSuccess, string message)
+        {
+            IsSuccess = isSuccess;
+            Message = message;
+        }
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+    }
 }
