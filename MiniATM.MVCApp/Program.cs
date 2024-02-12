31c218459bc5924e3fe91430a6b55c129b399372
ZinLinnhtet <mackk5504@gmail.com>
2024-02-13 03:16:36 +0630

    -

--- MiniATM.MVCApp/Program.cs
+++ MiniATM.MVCApp/Program.cs
@@ -1,16 +1,49 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using MiniATM.MVCApp.EFDbContext;
+using System.Net;
+using System.Text;
+using MiniATM.MVCApp.Middlewares;
+using MiniATM.MVCApp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
 builder.Services.AddDistributedMemoryCache();
 builder.Services.AddSession(options =>
 {
     options.IdleTimeout = TimeSpan.FromMinutes(30);
     options.Cookie.HttpOnly = true;
     options.Cookie.IsEssential = true;
+});
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options =>
+    {
+        options.RequireHttpsMetadata = false;
+        options.SaveToken = true;
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Issuer"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!))
+        };
+    });
+builder.Services.AddAuthorization(options =>
+{
+    options.DefaultPolicy = new AuthorizationPolicyBuilder()
+        .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
+        .RequireAuthenticatedUser()
+        .Build();
 });
 builder.Services.AddDbContext<AppDbContext>(options =>
     {
@@ -19,6 +52,11 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     },
     ServiceLifetime.Transient,
     ServiceLifetime.Transient);
+
+builder.Services.AddTransient<JwtAuthenticationMiddleware>();
+builder.Services.AddControllersWithViews();
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.AddScoped<MenuService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -31,9 +69,17 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+app.UseStatusCodePages(async context => {
+    var request = context.HttpContext.Request;
+    var response = context.HttpContext.Response;
 
+    if (response.StatusCode == (int)HttpStatusCode.Unauthorized)
+    {
+        response.Redirect("/login");
+    }
+});
 app.UseRouting();
-
+app.UseMiddleware<JwtAuthenticationMiddleware>();
 app.UseAuthorization();
 app.UseSession();
 app.MapControllerRoute(
