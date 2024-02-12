2e876649478226d34d5c1c5a6d392c251e6ad44a
ZinLinnhtet <mackk5504@gmail.com>
2024-02-13 03:16:22 +0630

    -

---
+++ MiniATM.MVCApp/Models/MenuModel.cs
@@ -0,0 +1,18 @@
+﻿namespace MiniATM.MVCApp.Models
+{
+    public class MenuModel
+    {
+        public string MainMenu { get; set; }
+        public string ManinMenuController { get; set; }
+        public string ActionName { get; set; }
+        public List<SubMenuModel> SubMenus { get; set; } = new List<SubMenuModel>();
+
+    }
+
+    public class SubMenuModel
+    {
+        public string SubMenu { get; set; }
+        public string SubMenuController { get; set;}
+        public string ActionName { get; set; }
+    }
+}
