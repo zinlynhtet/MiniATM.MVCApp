90dc792404419a2852558699cfb65e08201192b3
ZinLinnhtet <mackk5504@gmail.com>
2024-02-14 01:09:58 +0630

    -

--- MiniATM.MVCApp/Models/LoginDataModel.cs
+++ MiniATM.MVCApp/Models/LoginDataModel.cs
@@ -4,6 +4,7 @@ public class LoginDataModel
 {
     public string AdminUsername { get; set; }
     public string AdminPassword { get; set; }
+    public string UserId { get; set; }
     public int? CardNumber { get; set; }
     public string Password { get; set; }
 }
