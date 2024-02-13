a269e058481624d76de4e77ad8520d12982c371f
ZinLinnhtet <mackk5504@gmail.com>
2024-02-14 01:10:19 +0630

    -

---
+++ MiniATM.MVCApp/Services/TblRole.cs
@@ -0,0 +1,29 @@
+﻿using NUlid;
+
+namespace MiniATM.MVCApp.Services;
+
+public class TblRole
+{
+    public string RoleId { get; set; }
+    public string RoleCode { get; set; }
+    public string RoleName { get; set; }
+    public bool IsAdmin { get; set; }
+
+    public TblRole()
+    {
+
+    }
+    public TblRole(string roleCode, string roleName, bool isAdmin)
+    {
+        RoleId = Ulid.NewUlid().ToString();
+        RoleCode = roleCode;
+        RoleName = roleName;
+        IsAdmin = isAdmin;
+    }
+
+    public static List<TblRole> Roles = new List<TblRole>()
+    {
+        new TblRole("R001","User",false),
+        new TblRole("R002","Admin",true),
+    };
+}
